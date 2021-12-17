4f160d29a4b3a6cabdaba6c9a228eeeff1a6288f
ENDAVA\ischipala <ianko.schipala@endava.com>
2021-12-17 12:58:46 +0200

    create simple UI for camera preview. request access for camera. preview camera. added camera permission to Manifest.

--- app/src/main/java/uk/co/kidsloop/features/videostream/LiveVideoStreamFragment.kt
+++ app/src/main/java/uk/co/kidsloop/features/videostream/LiveVideoStreamFragment.kt
@@ -1,21 +1,111 @@
 package uk.co.kidsloop.app.features.videostream
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.fragment.app.viewModels
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import uk.co.kidsloop.R
 import uk.co.kidsloop.app.BaseFragment
 import uk.co.kidsloop.app.viewmodel.ViewModelFactory
+import uk.co.kidsloop.databinding.LiveVideostreamFragmentBinding
 import uk.co.kidsloop.features.videostream.LiveVideoStreamViewModel
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import javax.inject.Inject
 
-class LiveVideoStreamFragment: BaseFragment(R.layout.live_videostream_fragment) {
+class LiveVideoStreamFragment : BaseFragment(R.layout.live_videostream_fragment) {
 
-    @Inject lateinit var viewModelFactory: ViewModelFactory
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    private val binding by viewBinding(LiveVideostreamFragmentBinding::bind)
 
     private val viewModel: LiveVideoStreamViewModel by viewModels { viewModelFactory }
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         injector.inject(this)
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            activity?.let {
+                ActivityCompat.requestPermissions(
+                    it, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+                )
+            }
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
     }
+
+    private fun startCamera() {
+        val cameraProviderFuture = context?.let { ProcessCameraProvider.getInstance(it) }
+
+        cameraProviderFuture?.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview
+                )
+
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, context?.let { ContextCompat.getMainExecutor(it) })
+
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        context?.let { context ->
+            ContextCompat.checkSelfPermission(
+                context, it
+            )
+        } == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraX"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Log.e(TAG, "Access denied")
+            }
+        }
+    }
+
 }
