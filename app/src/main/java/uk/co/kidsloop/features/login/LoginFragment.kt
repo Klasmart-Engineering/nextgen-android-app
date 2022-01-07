3ad9c22e43a63e37d91a0fc77a881c40118ac824
Paul-Bisioc-Endava <97092637+Paul-Bisioc-Endava@users.noreply.github.com>
2022-01-07 09:40:10 +0200

    - include missing file

--- app/src/main/java/uk/co/kidsloop/features/login/LoginFragment.kt
+++ app/src/main/java/uk/co/kidsloop/features/login/LoginFragment.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.navigation.Navigation
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import uk.co.kidsloop.R
-import uk.co.kidsloop.app.BaseFragment
+import uk.co.kidsloop.app.structure.BaseFragment
 import uk.co.kidsloop.databinding.LoginFragmentBinding
 
 class LoginFragment : BaseFragment(R.layout.login_fragment) {
