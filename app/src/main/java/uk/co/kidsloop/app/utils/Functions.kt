133bfcd929bf5939eaa368bb9dcb90ec37075e53
Paul-Bisioc-Endava <97092637+Paul-Bisioc-Endava@users.noreply.github.com>
2022-01-07 09:45:51 +0200

    - add basic Extensions and Functions

---
+++ app/src/main/java/uk/co/kidsloop/app/utils/Functions.kt
@@ -0,0 +1,50 @@
+package uk.co.kidsloop.app.utils
+
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+/**
+ *  Created by paulbisioc on 07.01.2022
+ */
+
+// Long Toast functions
+fun Context.longToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun AppCompatActivity.longToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun Fragment.longToast(message: String) {
+    Toast.makeText(this.requireActivity(), message, Toast.LENGTH_LONG).show()
+}
+
+// Short Toast functions
+fun Context.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun AppCompatActivity.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.shortToast(message: String) {
+    Toast.makeText(this.requireActivity(), message, Toast.LENGTH_SHORT).show()
+}
+
+// View visibility functions
+fun View?.visible() {
+    this?.visibility = View.VISIBLE
+}
+
+fun View?.invisible() {
+    this?.visibility = View.INVISIBLE
+}
+
+fun View?.gone() {
+    this?.visibility = View.GONE
+}
