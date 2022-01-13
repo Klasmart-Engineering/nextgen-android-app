67e65538b14aac14dead8f2e9e023bd54b92f2a5
ENDAVA\Alapetroaei <alexandru.apetroaei@endava.com>
2022-01-13 13:03:45 +0200

    Removed unused import.

--- app/src/main/java/uk/co/kidsloop/features/liveclass/LiveClassFragment.kt
+++ app/src/main/java/uk/co/kidsloop/features/liveclass/LiveClassFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.marginEnd
 import androidx.fragment.app.viewModels
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
