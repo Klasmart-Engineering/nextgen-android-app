6e16397fb26a8327ae5fc982ca781bac62344d69
Paul-Bisioc-Endava <paul-robert.bisioc@endava.com>
2022-01-27 15:45:46 +0200

    - define DataChannelAction and DataChannelActionTypes

---
+++ app/src/main/java/uk/co/kidsloop/data/enums/DataChannelActionTypes.kt
@@ -0,0 +1,8 @@
+package uk.co.kidsloop.data.enums
+
+/**
+ *  Created by paulbisioc on 26.01.2022
+ */
+enum class DataChannelActionTypes(val type: String) {
+    RAISE_HAND("raise_hand")
+}
---
+++ app/src/main/java/uk/co/kidsloop/liveswitch/DataChannelAction.kt
@@ -0,0 +1,8 @@
+package uk.co.kidsloop.liveswitch
+
+/**
+ *  Created by paulbisioc on 25.01.2022
+ */
+interface DataChannelAction {
+    fun displayMessage(message: String)
+}
