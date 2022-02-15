d98a09a4e692e5e65b7caecaf2d717b7e6637b5b
Paul Bisioc <paulrobert.bisioc@endava.com>
2022-02-15 17:50:53 +0200

    - localise hardcoded values

--- app/src/main/java/uk/co/kidsloop/features/liveclass/localmedia/CameraLocalMedia.kt
+++ app/src/main/java/uk/co/kidsloop/features/liveclass/localmedia/CameraLocalMedia.kt
@@ -5,6 +5,7 @@ import android.view.View
 import fm.liveswitch.*
 import fm.liveswitch.android.Camera2Source
 import fm.liveswitch.android.CameraPreview
+import uk.co.kidsloop.data.enums.VideoQuality
 import uk.co.kidsloop.features.liveclass.remoteviews.AecContext
 
 class CameraLocalMedia(
@@ -23,16 +24,17 @@ class CameraLocalMedia(
             this.videoSimulcastEncodingCount = 3
 
             val videoEncodings = Array(3) { VideoEncodingConfig() }
-            videoEncodings[0].bitrate = 512
-            videoEncodings[0].frameRate = 30.0
+            videoEncodings[0].bitrate = VideoQuality.P720.videoBitrate
+            videoEncodings[0].frameRate = VideoQuality.P720.frameRate
+            videoEncodings[0].scale = VideoQuality.P720.scale
 
-            videoEncodings[1].bitrate = 256
-            videoEncodings[1].frameRate = 15.0
-            videoEncodings[1].scale = 0.5
+            videoEncodings[1].bitrate = VideoQuality.P480.videoBitrate
+            videoEncodings[1].frameRate = VideoQuality.P480.frameRate
+            videoEncodings[1].scale = VideoQuality.P480.scale
 
-            videoEncodings[2].bitrate = 128
-            videoEncodings[2].frameRate = 7.5
-            videoEncodings[2].scale = 0.25
+            videoEncodings[2].bitrate = VideoQuality.P320.videoBitrate
+            videoEncodings[2].frameRate = VideoQuality.P320.frameRate
+            videoEncodings[2].scale = VideoQuality.P320.scale
 
             this.videoSimulcastDisabled = false
             this.videoEncodings = videoEncodings
