c0c52ba383335ecc521e6cf840d58ede22d7aaf6
ENDAVA\Alapetroaei <alexandru.apetroaei@endava.com>
2022-03-08 16:25:06 +0200

    Set screen orientation portrait in manifest to make app strart without a screen rotation animation. Set orientation back to landscape  in "login" fragment.

--- app/src/main/java/uk/co/kidsloop/features/regionAndLanguage/LanguageFragment.kt
+++ app/src/main/java/uk/co/kidsloop/features/regionAndLanguage/LanguageFragment.kt
@@ -1,6 +1,5 @@
 package uk.co.kidsloop.features.regionAndLanguage
 
-import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,11 +10,6 @@ import uk.co.kidsloop.databinding.FragmentLanguageBinding
 
 class LanguageFragment : BaseFragment(R.layout.fragment_language) {
     private val binding by viewBinding(FragmentLanguageBinding::bind)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/uk/co/kidsloop/features/regionAndLanguage/RegionFragment.kt
+++ app/src/main/java/uk/co/kidsloop/features/regionAndLanguage/RegionFragment.kt
@@ -1,6 +1,5 @@
 package uk.co.kidsloop.features.regionAndLanguage
 
-import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.Navigation
@@ -12,11 +11,6 @@ import uk.co.kidsloop.databinding.FragmentRegionBinding
 
 class RegionFragment : BaseFragment(R.layout.fragment_region) {
     private val binding by viewBinding(FragmentRegionBinding::bind)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
