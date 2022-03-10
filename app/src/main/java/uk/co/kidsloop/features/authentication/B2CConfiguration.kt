d2cbfedb22adfb44dc488749b973b624a764ab9d
enalupescu <adrian.lupescu@endava.com>
2022-03-10 13:30:39 +0200

    Added code review asked changes

--- app/src/main/java/uk/co/kidsloop/features/authentication/B2CConfiguration.kt
+++ app/src/main/java/uk/co/kidsloop/features/authentication/B2CConfiguration.kt
@@ -3,14 +3,6 @@ package uk.co.kidsloop.features.authentication
 import java.util.*
 
 object B2CConfiguration {
-    /**
-     * Name of the policies/user flows in your B2C tenant.
-     * See https://docs.microsoft.com/en-us/azure/active-directory-b2c/active-directory-b2c-reference-policies for more info.
-     */
-    val Policies = arrayOf(
-        "b2c_1a_relying_party_sign_up_log_in",
-        "b2c_1a_profileedit"
-    )
 
     /**
      * Name of your B2C tenant hostname.
@@ -36,7 +28,7 @@ object B2CConfiguration {
      * These scopes must be added in your B2C application page.
      */
     val scopes: List<String>
-        get() = Arrays.asList(
+        get() = listOf(
             "https://login.sso.kidsloop.live/010eb29e-d42b-4ca3-9c16-1961a528ce77/tasks.read",
             "https://login.sso.kidsloop.live/010eb29e-d42b-4ca3-9c16-1961a528ce77/tasks.write"
         )
