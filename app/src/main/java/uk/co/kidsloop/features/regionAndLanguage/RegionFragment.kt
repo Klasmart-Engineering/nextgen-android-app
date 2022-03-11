403ac719fc7a07d5376a8ae6bf264e9320a7d274
ENDAVA\Alapetroaei <alexandru.apetroaei@endava.com>
2022-03-11 17:22:49 +0200

    Removed unused import.

--- app/src/main/java/uk/co/kidsloop/features/regionAndLanguage/RegionFragment.kt
+++ app/src/main/java/uk/co/kidsloop/features/regionAndLanguage/RegionFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.util.Pair
 import android.view.View
-import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.navigation.Navigation
